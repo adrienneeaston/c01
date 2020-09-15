77fcd2298837968abaf2033f28be69adb8f46826
Adrienne <adrienneeaston@gmail.com>
2020-09-14 22:19:12 -0700

    finish demo

--- js/get-product.js
+++ js/get-product.js
@@ -1,14 +1,20 @@
 'use strict'
 
-var response = prompt('Would you rather have a house or hotel?');
-var message;
 
-if (response === 'house') {
-  message = 'Houses are nice!';
-} else if (response === 'hotel') {
-  message = 'Hotels are nice!';
-} else {
-  message = 'Fine';
+
+function getProduct() {
+  var response = prompt('Would you rather have a house or hotel?');
+  var message;
+
+  if (response === 'house') {
+    message = 'Houses are nice!';
+  } else if (response === 'hotel') {
+    message = 'Hotels are nice!';
+  } else {
+    message = 'Fine';
+  }
+
+  return document.write('<h3>' + message + '</h3>');
 }
 
-document.write('<h3>' + message + '</h3>');
+getProduct();
